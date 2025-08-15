f8283691a284f354a15b8162ff1c6a083e1c2253
dandan <xhq1897@163.com>
2025-08-15 21:53:46 +0800

    解决请求错误

--- ruoyi-ui/src/utils/request.js
+++ ruoyi-ui/src/utils/request.js
@@ -16,7 +16,7 @@ axios.defaults.headers["Content-Type"] = "application/json;charset=utf-8";
 const service = axios.create({
   // axios中请求配置有baseURL选项，表示请求URL公共部分
   baseURL: process.env.VUE_APP_BASE_API,
-  baseURL: "http://192.168.1.203:8089/",
+  // baseURL: "http://192.168.1.203:8089/",
   // 超时
   timeout: 10000,
 });
